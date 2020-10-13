fb9e7f77c6e23fbca0fc798eddab9f6b08f2fdb9
peteri gabor <peteri@t-online.hu>
2020-10-13 18:41:48 +0200

    paros szamok osszege es mennyisege

--- src/kutyagumi.java
+++ src/kutyagumi.java
@@ -6,15 +6,65 @@ public class kutyagumi {
             tomb[i]=(int)((Math.random()*100)+1);
         }
         System.out.println("a legkisebb ertek: "+minimum(tomb));
+        System.out.println(" A tömb legnagyobb eleme: " + maxOfArray(tomb));
+        System.out.println(" A páratlan elemek száma: " + oddsNumbersInArray(tomb));
+        System.out.println("a paros szamok osszege: "+sumOfEvens(tomb));
+        System.out.println("a paratlan szamok mennyisege: "+evenNumbersInArray(tomb));
+
     }
 
     public static int minimum(int[] tomb){
         int min=Integer.MAX_VALUE;
         for (int i = 0; i < tomb.length; i++) {
             if(tomb[i]<min){
-             min=tomb[i];
+                min=tomb[i];
             }
         }
         return min;
     }
+    public static int maxOfArray ( int [] nums) {
+        int max= 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if ( max < nums [i]) {
+                max = nums [i];
+            }
+
+        }
+        return max;
+    }
+    public static int oddsNumbersInArray(int[] nums) {
+
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 2 != 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static int evenNumbersInArray(int[] tomb){
+        int counter=0;
+        for (int i = 0; i < tomb.length; i++) {
+            if(tomb[i]%2==0){           //feltetelezzuk, hogy a 0 is paros szam:)
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static int sumOfEvens(int[] tomb){
+        int sum=0;
+        for (int i = 0; i < tomb.length ; i++) {
+            if(tomb[i]%2==0){
+                sum+=tomb[i];
+            }
+        }
+        return sum;
+    }
+
+
 }
